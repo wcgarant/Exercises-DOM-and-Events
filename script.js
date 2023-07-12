8133975e92f88c7260e591ec54b522528e0d2c10
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-07-12 15:59:30 -0400

    edited event listeners for click functions to use const

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+  button.addEventListener("click", function(event) {
+    paragraph.innerHTML = "Houston, we have liftoff!"
+  });
+
+  missionAbort.addEventListener("mouseover", function(event) {
+    event.target.style.backgroundColor = 'red';
+  });
+
+  missionAbort.addEventListener("mouseleave", function(event) {
+    event.target.style.backgroundColor = '';
+  });
+
+  missionAbort.addEventListener("click", function(event) {
+    if (window.confirm("Are you sure you want to abort the mission?")) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home."
+    }
+  });
     
 }
 
